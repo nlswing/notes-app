0f99f7f0edce11420a136410a2583dcb14705fea
Nathan Wing <wing_nathan@hotmail.com>
2023-05-02 16:33:25 +0100

    Project setup

---
+++ bundle.js
@@ -0,0 +1,4 @@
+// bundle.js is the name of the bundle file that will be loaded by the browser.
+
+(() => {
+})();
---
+++ index.js
@@ -0,0 +1,2 @@
+// index.js is the main file of our JavaScript program. 
+// In other words, is the file that requires other files, but that is not required by any other.
